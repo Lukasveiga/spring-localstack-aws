9ba8b82fa0f87b6b81359722452d77f8f02e9007
Lukas Veiga <lukas.veiga@hotmail.com>
2024-08-15 10:13:23 -0300

    feat: aws secrets manager

--- src/main/java/com/lukasdev/spring_localstack_up/secretsmanager/config/SecretsManagerConfig.java
+++ src/main/java/com/lukasdev/spring_localstack_up/secretsmanager/config/SecretsManagerConfig.java
@@ -1,4 +1,29 @@
 package com.lukasdev.spring_localstack_up.secretsmanager.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SecretsManagerConfig {
+
+    @Value("${valor1}")
+    private String valor1;
+
+    @Value("${valor2}")
+    private String valor2;
+
+    @Value("${valor3}")
+    private String valor3;
+
+    public String getValor1() {
+        return valor1;
+    }
+
+    public String getValor2() {
+        return valor2;
+    }
+
+    public String getValor3() {
+        return valor3;
+    }
 }
--- src/main/java/com/lukasdev/spring_localstack_up/secretsmanager/controller/SecretsManagerController.java
+++ src/main/java/com/lukasdev/spring_localstack_up/secretsmanager/controller/SecretsManagerController.java
@@ -1,4 +1,28 @@
 package com.lukasdev.spring_localstack_up.secretsmanager.controller;
 
+import com.lukasdev.spring_localstack_up.secretsmanager.config.SecretsManagerConfig;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class SecretsManagerController {
+
+    private final SecretsManagerConfig secretsManagerConfig;
+
+    public SecretsManagerController(SecretsManagerConfig secretsManagerConfig) {
+        this.secretsManagerConfig = secretsManagerConfig;
+    }
+
+    @GetMapping("/secrets-manager")
+    public ResponseEntity<String> getSecrets() {
+        return ResponseEntity.ok(
+                String.format("%s - %s - %s",
+                        this.secretsManagerConfig.getValor1(),
+                        this.secretsManagerConfig.getValor2(),
+                        this.secretsManagerConfig.getValor3())
+        );
+    }
 }
